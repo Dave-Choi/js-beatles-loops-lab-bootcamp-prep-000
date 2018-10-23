9e472b873c64ef766a0300fca1c135b0f7ae82bd
David Choi <daaave@gmail.com>
2018-10-23 19:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  let output = [];
+  let numMusicians = musicians.length;
+  
+  for(let i=0; i<numMusicians; i++){
+    let musician = musicians[i];
+    let instrument = instruments[i];
+    output.push(`${musician} plays ${instrument}.`);
+  }
+  
+  return output;
+}
